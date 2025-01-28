72ded7790c0983fd47e85548e8fac7c4ec0742e7
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2025-01-28 22:06:27 +0545

    user authentication controllers and routes

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import connectDB from './db.js';  // Import the DB connection
+import dotenv from 'dotenv';
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRouter from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js';
+
+const app = express();
+const PORT = 8000;
+
+// Connect to MongoDB
+connectDB();
+
+app.use(express.urlencoded({ extended: true })); // For form-data (application/x-www-form-urlencoded)
+app.use(express.json()); // Middleware to parse JSON data
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+app.get('/', (req, res) => {
+  res.send("API Working")
+});
+
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
