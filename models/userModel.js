a0bf5898cebcbea6fd75a152eec1b0edbf12a55f
Kshitij Adhikari <np03cs4s230250@heraldcollege.edu.np>
2025-03-02 12:52:54 +0545

    updated user schema to store otp

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,12 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: 'user' },  // Role field (user or vendor)
+    verifyOtp: { type: String, default: "" },
+    verifyOtpExpireAt: { type: Number, default: 0 },
+    isAccountVerified: { type: Boolean, default: false },
+    resetOtp: { type: String, default: "" },
+    resetOtpExpireAt: { type: Number, default: 0 },
+    role: { type: String, default: 'user' },  // Role field (user or vendor or admin)
     organization: { type: String, default: "" },  // Vendor-specific fields
     contact: { type: String, default: "" },
     address: { type: String, default: "" },
